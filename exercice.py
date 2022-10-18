2ba17d3e7f90341d73e859af0ea14ddf5a1cfccb
fabricerenard12 <fabrice.renard12@outlook.com>
2022-10-17 22:43:31 -0400

    Exercices complétés

--- exercice.py
+++ exercice.py
@@ -3,44 +3,60 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	return pow(voltage, 2) / resistance
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	if (v1[0] * v2[0] + v1[1] * v2[1]) == 0:
+		return True
+	return False
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	res = 0
+	count = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >= 0:
+			res += v
+			count += 1
+			
+	return res / count # La variable v contient une valeur de la liste.
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties, tens, fives, ones = 0, 0, 0, 0
 	while value != 0:
 		if value >= 20:
-			pass
+			twenties += 1
+			value -= 20
 		elif value >= 10:
-			pass
+			tens += 1
+			value -= 10
 		elif value >= 5:
-			pass
+			fives += 1
+			value -= 5
 		elif value >= 1:
-			pass
+			ones += 1
+			value -= 1
 
-	return (twenties, tens, fives, twos, ones);
+	return (twenties, tens, fives, ones);
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
 	# `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
 	result = ""
 	abs_value = abs(value)
+
 	while abs_value != 0:
-		pass
+		digit = abs_value % base
+		result += digit_letters[digit]
+		abs_value //= base
+	
 	if value < 0:
 		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-		pass
-	return result
+		result += '-'
+
+	return result[::-1]
 
 
 if __name__ == "__main__":
